404adab118a75f65596b1d37e58b11b46328a3ba
Sb1414 <kalkamanova.sabina14@gmail.com>
2024-09-21 11:51:14 +0300

    first

---
+++ uni_python/myproject/myapp/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('<h1> Hello world </h1>')
+
+def about(request):
+    return HttpResponse('<h2> Hello world </h2>')
