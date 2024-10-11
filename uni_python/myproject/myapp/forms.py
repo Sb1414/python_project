029b017c0d49f5f9824ab05ac64e3c805b1f0087
Sb1414 <kalkamanova.sabina14@gmail.com>
2024-10-11 13:29:33 +0300

    add forms, views, models

---
+++ uni_python/myproject/myapp/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import Attraction, Category, Event
+
+
+class AttractionForm(forms.ModelForm):
+    class Meta:
+        model = Attraction
+        fields = ['name', 'description', 'price', 'category']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ['name', 'description', 'date', 'time', 'attraction']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'time': forms.TimeInput(attrs={'class': 'form-control', 'type': 'time'}),
+            'attraction': forms.Select(attrs={'class': 'form-control'}),
+        }
--- uni_python/myproject/myapp/views.py
+++ uni_python/myproject/myapp/views.py
@@ -1,9 +1,12 @@
 from django.contrib.sites import requests
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 import requests
 from datetime import datetime
 from .github_api import get_github_data
+from .forms import AttractionForm, CategoryForm, EventForm
+from .models import Attraction, Category, Event
+
 
 def index(request):
     return render(request, 'myapp/index.html')
@@ -15,3 +18,102 @@ def info(request):
     username = 'Sb1414'
     github_data = get_github_data(username)
     return render(request, 'myapp/info.html', {'github_data': github_data})
+
+def attraction_list(request):
+    attractions = Attraction.objects.all()
+    form = AttractionForm()
+    return render(request, 'myapp/attraction_list.html', {'attractions': attractions, 'form': form})
+
+def add_attraction(request):
+    if request.method == 'POST':
+        form = AttractionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('attraction_list')
+    else:
+        form = AttractionForm()
+    return render(request, 'myapp/add_attraction.html', {'form': form})
+
+def edit_attraction(request, pk):
+    attraction = get_object_or_404(Attraction, pk=pk)
+    if request.method == 'POST':
+        form = AttractionForm(request.POST, instance=attraction)
+        if form.is_valid():
+            form.save()
+            return redirect('attraction_list')
+    else:
+        form = AttractionForm(instance=attraction)
+    return render(request, 'myapp/edit_attraction.html', {'form': form})
+
+def delete_attraction(request, pk):
+    attraction = get_object_or_404(Attraction, pk=pk)
+    if request.method == 'POST':
+        attraction.delete()
+        return redirect('attraction_list')
+    return render(request, 'myapp/delete_attraction.html', {'attraction': attraction})
+
+def category_list(request):
+    categories = Category.objects.all()
+    form = CategoryForm()
+    return render(request, 'myapp/category_list.html', {'categories': categories, 'form': form})
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm()
+    return render(request, 'myapp/add_category.html', {'form': form})
+
+def edit_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'myapp/edit_category.html', {'form': form})
+
+def delete_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('category_list')
+    return render(request, 'myapp/delete_category.html', {'category': category})
+
+def event_list(request):
+    events = Event.objects.all()
+    form = EventForm()
+    return render(request, 'myapp/event_list.html', {'events': events, 'form': form})
+
+def add_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('event_list')
+    else:
+        form = EventForm()
+    return render(request, 'myapp/add_event.html', {'form': form})
+
+def edit_event(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            form.save()
+            return redirect('event_list')
+    else:
+        form = EventForm(instance=event)
+    return render(request, 'myapp/edit_event.html', {'form': form})
+
+def delete_event(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    if request.method == 'POST':
+        event.delete()
+        return redirect('event_list')
+    return render(request, 'myapp/delete_event.html', {'event': event})
